72878d6f6185a9fe2b52bcfc6767fa283588ed25
khaly <khalyavinartem@yandex.ru>
2021-05-17 17:28:03 +0300

    удалил EntityManager em; вынес UserDetailsService userService в отдельную директорию; заменил Eager на Lazyб добавил к @EnableTransactionManager(proxyTargetClass = true), добавил аннотацию у метода Authentication authentication @Transactional Добавил интерфейс UserService и через него вызвал MyUserDetailService

--- src/main/java/web/security/MyUserDetailService.java
+++ src/main/java/web/security/MyUserDetailService.java
@@ -4,24 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import web.model.Role;
 import web.model.User;
 import web.repository.RoleRepository;
 import web.repository.UserRepository;
-import java.util.Collections;
-import java.util.List;
 
 
-@Service
+
+@Component
 public class MyUserDetailService implements UserDetailsService {
 
 
-    @Autowired
     UserRepository userRepository;
+
+    public MyUserDetailService(){}
+
     @Autowired
-    RoleRepository roleRepository;
+    public MyUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -35,40 +37,4 @@ public class MyUserDetailService implements UserDetailsService {
         );
     }
 
-    public User findUserById(Long id){
-        return userRepository.findById(id).get();
-    }
-
-    public boolean saveUser(User user){
-        User userFromBd = null;
-        if(userRepository.findById(user.getId()).isPresent()) {
-            userFromBd = userRepository.findById(user.getId()).get();
-        }
-        if(userFromBd == null){
-            return false;
-        }
-        user.setRoles(Collections.singleton(new Role(2L,"USER")));
-        userRepository.save(user);
-        return true;
-    }
-
-    public boolean deleteUser(Long id){
-        if(userRepository.findById(id).isPresent()){
-            userRepository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
-
-    public List<User> userList(){
-        return userRepository.findAll();
-    }
-
-    public void addUser(User user){
-        userRepository.save(user);
-    }
-
-    public User findByUsername(String name){
-        return userRepository.findByUsername(name);
-    }
 }
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,48 +2,63 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.security.MyUserDetailService;
+import web.model.Role;
 import web.model.User;
+import web.repository.UserRepository;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    MyUserDetailService myUserDetailService;
+    UserRepository userRepository;
 
     @Autowired
-    public void setMyUserDetailService(MyUserDetailService myUserDetailService) {
-        this.myUserDetailService = myUserDetailService;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findUserById(Long id){
+        return userRepository.findById(id).get();
     }
 
     @Override
-    public User findUserById(Long id) {
-        return myUserDetailService.findUserById(id);
+    public boolean saveUser(User user){
+        User userFromBd = null;
+        if(userRepository.findById(user.getId()).isPresent()) {
+            userFromBd = userRepository.findById(user.getId()).get();
+        }
+        if(userFromBd == null){
+            return false;
+        }
+        user.setRoles(Collections.singleton(new Role(2L,"USER")));
+        userRepository.save(user);
+        return true;
     }
 
     @Override
-    public boolean saveUser(User user) {
-        return myUserDetailService.saveUser(user);
+    public boolean deleteUser(Long id){
+        if(userRepository.findById(id).isPresent()){
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public boolean deleteUser(Long id) {
-        return myUserDetailService.deleteUser(id);
+    public List<User> userList(){
+        return userRepository.findAll();
     }
 
     @Override
-    public List<User> userList() {
-        return myUserDetailService.userList();
+    public void addUser(User user){
+        userRepository.save(user);
     }
 
     @Override
-    public void addUser(User user) {
-        myUserDetailService.addUser(user);
+    public User findByUserName(String name){
+        return userRepository.findByUsername(name);
     }
 
-    @Override
-    public User findByUserName(String name) {
-        return myUserDetailService.findByUsername(name);
-    }
 }
